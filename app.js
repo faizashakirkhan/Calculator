19f2449cdb9e3b3401a5fff2eb1ace93fc6560f3
faizashakirkhan <faizashakirkhan0@gamil.com>
2024-08-14 23:29:53 +0500

    Message

---
+++ app.js
@@ -0,0 +1,20 @@
+var display=document.getElementById('display')
+
+function indexDisplay(input){
+display.value+=input
+}
+
+function clearDisplay(){
+    display.value="";
+
+}
+
+function calculate(){
+    try{
+        display.value=eval(display.value)
+    }
+    catch(error){
+display.value=error;
+    }
+    
+}
